67383fa1570f3c07296f06747f68d2fa0b17eed3
GasparGomez <a21gasgomtor@inspedralbes.cat>
2022-10-07 13:16:00 +0200

    local prova

--- funcionsMVC.js
+++ funcionsMVC.js
@@ -38,16 +38,25 @@ let data = {
     ]
 };
 
-var model = { // si no estÃ¡ inicializado el localstorage lo arrancas
+var model = {
+    // si no estÃ¡ inicializado el localstorage lo arrancas
     init: function () {
+        if (localStorage.getItem('datos_gatos') == null) {
+            localStorage.setItem('datos_gatos', JSON.stringify(data.gatos));
+        } else {
+            data.gatos = JSON.parse(localStorage.getItem('datos_gatos'));
+        }
+        
         console.log("datos preparados");
     },
+
     getGatos: function () {
-        return data.gatos;
+        return JSON.parse(localStorage.getItem('datos_gatos'));
     },
 
     setCatActive: function (n) {
         data.gatoActivo = n;
+        localStorage.setItem('gato_activo', n);
     },
     getGatoActivo: function () {
         return data.gatos[data.gatoActivo];
@@ -84,8 +93,7 @@ var controller = {
 };
 
 
-var view = {
-    // Demanar dades al controller
+var view = { // Demanar dades al controller
 
     init: function () { // 1. Pintar llista
         gatos = controller.getGatos();
@@ -99,15 +107,14 @@ var view = {
         }
         document.getElementById("llista").innerHTML = htmlStr;
 
-        //2 .poner listenr de lista 
+        // 2 .poner listenr de lista
         document.getElementById("llista").addEventListener("click", function (e) {
-            console.log(e.target);
             n = e.target.id;
             controller.clickLista(n);
-            
+
         });
 
-        //3. pongo el listener del gato
+        // 3. pongo el listener del gato
         document.getElementById("imgGato").addEventListener("click", function () {
             controller.clickGato();
         });
@@ -133,87 +140,3 @@ var view = {
 };
 
 controller.init();
-
-/*
-let data = {
-    gatoActivo: 0,
-    gatos: [
-        {
-            name: "Ismael",
-            content: "gato ismael gato ismael",
-            image: "img/floppa.jpg",
-            nclicks: 0
-        },
-        {
-            name: "Oscar",
-            content: "ole el oscar",
-            image: "img/oscar.png",
-            nclicks: 0
-        },
-        {
-            name: "Fabian",
-            content: "fabian mangui :)",
-            image: "img/fabian.jpg",
-            nclicks: 0
-        },
-        {
-            name: "Gomi",
-            content: "mapache",
-            image: "img/gomielmapache.jpg",
-            nclicks: 0
-        }, {
-            name: "Marti",
-            content: "literalmente marti",
-            image: "img/bingus.jpg",
-            nclicks: 0
-        }, {
-            name: "Jaime",
-            content: "literalmente jaime",
-            image: "img/jaime.jpg",
-            nclicks: 0
-        }
-    ]
-};
-
-function addCats() {
-    htmlStr = "";
-    for (let i = 0; i < data.gatos.length; i++) {
-        htmlStr += `<li id="${i}">`;
-
-        htmlStr += data.gatos[i].name;
-
-        htmlStr += `</li>`;
-    }
-    document.getElementById("llista").innerHTML = htmlStr;
-}
-addCats();
-
-pintarCatActive();
-
-let gato = document.getElementById("llista");
-
-gato.addEventListener("click", function (e) {
-    console.log(e.target);
-    data.gatoActivo = e.target.id;
-    pintarCatActive();
-});
-
-function pintarCatActive() {
-        let image = document.getElementById("imgGato");
-
-        image.setAttribute("src", data.gatos[data.gatoActivo].image);
-        document.getElementById("nombreGato").innerText = data.gatos[data.gatoActivo].name;
-
-        pintarContador(data.gatoActivo);
-}
-
-function pintarContador(idG) {
-    let contador = document.getElementById("contador").innerText = data.gatos[idG].nclicks.toString();
-}
-
-let image = document.getElementById("imgGato");
-image.addEventListener("click", function (e) {
-    data.gatos[data.gatoActivo].nclicks++;
-    pintarContador(data.gatoActivo);
-});
-*/
